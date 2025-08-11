612747486b880769b1ddb5c87ae9aaa8831d47bf
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-08-11 09:52:52 +0800

    Component Text Header Lanjutan

--- core/ui/src/main/java/com/example/ui/components/TextUiJello.kt
+++ core/ui/src/main/java/com/example/ui/components/TextUiJello.kt
@@ -1,27 +1,37 @@
 package com.example.ui.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun JelloTextHeader(
-
+    text: String = "Welcome to Login",
+    color: Color = Color.Black,
+    modifier: Modifier = Modifier.padding(16.dp)
 ) {
     Text(
-        "Welcome to Login",
+        text,
         style = TextStyle(
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             color = Color.Black,
             lineHeight = 24.sp,
-            textAlign = TextAlign.Left
-        )
+            textAlign = TextAlign.Left,
+        ),
+        modifier = modifier,
+        color = color,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
     )
 }
 
