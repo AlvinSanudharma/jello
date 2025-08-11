eaf2699c4661a440675c29205d9e045a90e3cc4d
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-08-11 10:09:07 +0800

    Component Text Desk Click

--- core/ui/src/main/java/com/example/ui/components/TextUiJello.kt
+++ core/ui/src/main/java/com/example/ui/components/TextUiJello.kt
@@ -1,14 +1,18 @@
 package com.example.ui.components
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,8 +39,41 @@ fun JelloTextHeader(
     )
 }
 
+@Composable
+fun JelloTextRegularWithClick() {
+    val annotatedText = buildAnnotatedString {
+        append("Please fill E-mail & password to login your app account.")
+
+        pushStringAnnotation(
+            tag = "TEXT_CLICK",
+            annotation = "Sign Up"
+        )
+
+        withStyle(
+            style = SpanStyle(
+                color = Color.Blue,
+                fontWeight = FontWeight.Bold
+            )
+        ) {
+            append(" Sign Up")
+        }
+
+        pop()
+    }
+
+    Text(
+        text = annotatedText
+    )
+}
+
 @Preview(apiLevel = 34)
 @Composable
 fun JelloTextHeaderPreview() {
     JelloTextHeader()
+}
+
+@Preview(apiLevel = 34)
+@Composable
+fun JelloTextRegularWithClickPreview() {
+    JelloTextRegularWithClick()
 }
