5ab7b7f77f850ee750726606a3637f7def8f680b
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-08-28 20:04:37 +0800

    Component Button Primary

---
+++ core/ui/src/main/java/com/example/ui/components/ButtonUiJello.kt
@@ -0,0 +1,38 @@
+package com.example.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ui.theme.LightOrange
+import com.example.ui.theme.VeryDarkGrayistBlue
+
+@Composable
+fun JelloButtonPrimary(
+    text: String = "Login Now",
+    onClick: () -> Unit = {}
+) {
+    JelloBaseButton(
+        text = text,
+        onClick = onClick,
+        enable = true,
+        modifier = Modifier.fillMaxWidth()
+            .padding(16.dp)
+            .height(56.dp),
+        color = ButtonDefaults.buttonColors(
+            containerColor = LightOrange,
+            contentColor = VeryDarkGrayistBlue
+        )
+    )
+}
+
+@Preview(apiLevel = 34)
+@Composable
+fun JelloButtonPrimaryPreview() {
+    JelloButtonPrimary()
+}
